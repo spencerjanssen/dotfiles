a25d7b93fdc4e6529982266ac47767cbe8f23c6c
Spencer Janssen <spencerjanssen@gmail.com>
2015-08-11 13:58:04 -0500

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,59 @@
-import XMonad
-import XMonad.Config.Sjanssen
+import XMonad hiding (Tall(..))
+import System.Taffybar.Hooks.PagerHints
+import qualified XMonad.StackSet as W
+import XMonad.Actions.CopyWindow
+import XMonad.Layout.Tabbed
+import XMonad.Layout.HintedTile
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Prompt
+import XMonad.Actions.SpawnOn
+import XMonad.Util.SpawnOnce
 
-main = xmonad sjanssenConfig { terminal = "gnome-terminal" }
+import XMonad.Layout.LayoutScreens
+import XMonad.Layout.TwoPane
+
+import qualified Data.Map as M
+
+main = xmonad . pagerHints . ewmh $ defaultConfig
+    { terminal = "gnome-terminal"
+    , workspaces = ["irc", "web"] ++ map show [3 .. 8 :: Int] ++ ["tv"]
+    , mouseBindings = \(XConfig {modMask = modm}) -> M.fromList $
+            [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w))
+            , ((modm, button2), (\w -> focus w >> windows W.swapMaster))
+            , ((modm.|. shiftMask, button1), (\w -> focus w >> mouseResizeWindow w)) ]
+    , keys = \c -> mykeys c `M.union` keys defaultConfig c
+    , layoutHook = modifiers layouts
+    , manageHook = manageHook defaultConfig
+                <+> manageDocks
+                <+> manageSpawn
+                <+> (isFullscreen --> doFullFloat)
+                <+> (className =? "Kodi" --> doShift "tv")
+    }
+ where
+    tiled     = HintedTile 1 0.03 0.5 TopLeft
+    layouts   = (tiled Tall ||| (tiled Wide ||| Full)) ||| tabbed shrinkText myTheme
+    modifiers = avoidStruts . smartBorders
+
+    mykeys (XConfig {modMask = modm}) = M.fromList $
+        [((modm,               xK_p     ), shellPromptHere myPromptConfig)
+        ,((modm .|. shiftMask, xK_Return), spawnHere =<< asks (terminal . config))
+        ,((modm .|. shiftMask, xK_c     ), kill1)
+        ,((modm .|. shiftMask .|. controlMask, xK_c     ), kill)
+        ,((modm .|. shiftMask, xK_0     ), windows $ copyToAll)
+        ,((modm,               xK_z     ), layoutScreens 2 $ TwoPane 0.5 0.5)
+        ,((modm .|. shiftMask, xK_z     ), rescreen)
+        , ((modm             , xK_b     ), sendMessage ToggleStruts)
+        ]
+
+    myFont = "xft:Bitstream Vera Sans Mono:pixelsize=10"
+    myTheme = defaultTheme { fontName = myFont }
+    myPromptConfig = defaultXPConfig
+                        { position = Top
+                        , font = myFont
+                        , showCompletionOnTab = True
+                        , historyFilter = deleteConsecutive
+                        , promptBorderWidth = 0 }
