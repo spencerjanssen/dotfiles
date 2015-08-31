e8b1ad824fd48848706731ceee2a8d6833220075
Spencer Janssen <spencerjanssen@gmail.com>
2015-08-30 23:44:46 -0500

    Fix nix daily updating (I think)

--- nixos/update_daily.nix
+++ nixos/update_daily.nix
@@ -2,16 +2,17 @@
 {
     systemd.services.update_nix = {
         description = "Update nix channel and build system";
+        path = [ pkgs.nix ];
         serviceConfig = {
             Type = "oneshot";
             User = "root";
             # cd /tmp so that we don't leave a result symlink in an obnoxious
             # location
             ExecStart = ''
-                cd /tmp &&
-                /run/current-system/sw/bin/nix-channel --update &&
-                /run/current-system/sw/bin/nixos-rebuild build
+                /bin/sh -c "${pkgs.nix}/bin/nix-channel --update" ; \
+                /bin/sh -c "/run/current-system/sw/bin/nixos-rebuild build"
             '';
+            Environment = "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt NIX_PATH=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
         };
         startAt = "05:00";
     };
