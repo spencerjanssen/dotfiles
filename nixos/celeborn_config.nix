9dfe493f15c0b164a737f736de80e47acf2e1433
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-09 12:18:57 -0500

    Keep celeborn's hardware config in git

--- nixos/celeborn_config.nix
+++ nixos/celeborn_config.nix
@@ -4,7 +4,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      ./celeborn-hardware-configuration.nix
       ./flexgetservice.nix
       ./packages.nix
       ./users.nix
