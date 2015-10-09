b8ad3d9f9513f35d8a9731bbde74a7324aeae8b6
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-09 14:00:24 -0500

    Use celeborn as a binary cache for eee

--- nixos/eee_configuration.nix
+++ nixos/eee_configuration.nix
@@ -45,6 +45,15 @@ chainloader +1
   services.wakeonlan.interfaces = [ { interface = "enp1s0"; method = "magicpacket"; } ];
   services.xserver.synaptics.enable = true;
 
+  nix.binaryCaches = [
+    "http://192.168.1.200"
+    "https://cache.nixos.org/"
+  ];
+
+  nix.binaryCachePublicKeys = [
+    "celeborn-1:aECgTP22b6+Ar1lUfjBY2EwwoEp19+Nxr9FUMXkZ3gk="
+  ];
+
   # nix.extraOptions = "ssh-substituter-hosts = nix-ssh@192.168.1.200";
   nix.distributedBuilds = false;
   # nix.buildMachines = [ { hostName = "192.168.1.200"; maxJobs = 4; sshKey = "/home/sjanssen/.ssh/id_rsa"; sshUser = "sjanssen"; system = "x86_64-linux"; } ];
