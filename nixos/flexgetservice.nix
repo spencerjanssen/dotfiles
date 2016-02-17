122f46496210dd1fbf1b7de0e39671a973da60a5
Spencer Janssen <spencerjanssen@gmail.com>
2016-02-17 00:55:09 -0600

    Disable flexget service for now

--- nixos/flexgetservice.nix
+++ nixos/flexgetservice.nix
@@ -1,26 +1,26 @@
 { pkgs, ...}:
 
 {
-    systemd.services.flexget =
-    let myflexget = (pkgs.lib.overrideDerivation pkgs.python27Packages.flexget (attrs: {
-            propagatedBuildInputs = [pkgs.python27Packages.deluge];
-        }));
-    in {
-        wantedBy = [ "multi-user.target" ];
-        after = [ "network.target" "local-fs.target" "deluge.service"];
-        description = "flexget";
-        path = [ pkgs.python27Packages.flexget
-            pkgs.python27Packages.deluge
-            pkgs.deluge
-            ];
-        serviceConfig = {
-            Type = "simple";
-            User = "sjanssen";
-            ExecStart = ''${myflexget}/bin/flexget daemon start'';
-            ExecStop = ''${myflexget}/bin/flexget daemon stop''; 
-            ExecReload = ''${myflexget}/bin/flexget daemon reload'';
-        };
-    };
+#     systemd.services.flexget =
+#     let myflexget = (pkgs.lib.overrideDerivation pkgs.pythonPackages.flexget (attrs: {
+#             propagatedBuildInputs = [pkgs.pythonPackages.deluge pkgs.deluge ];
+#         }));
+#     in {
+#         wantedBy = [ "multi-user.target" ];
+#         after = [ "network.target" "local-fs.target" "deluge.service"];
+#         description = "flexget";
+#         path = [ myflexget
+#             pkgs.pythonPackages.deluge
+#             pkgs.deluge
+#             ];
+#         serviceConfig = {
+#             Type = "simple";
+#             User = "sjanssen";
+#             ExecStart = ''${myflexget}/bin/flexget daemon start'';
+#             ExecStop = ''${myflexget}/bin/flexget daemon stop''; 
+#             ExecReload = ''${myflexget}/bin/flexget daemon reload'';
+#         };
+#     };
 
     systemd.services.deluge = {
         wantedBy = [ "multi-user.target" ];
