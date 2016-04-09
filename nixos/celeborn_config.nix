bcc07d6621ba27c09c9686e901d10c4a86001f2a
Spencer Janssen <spencerjanssen@gmail.com>
2016-04-09 02:33:37 -0500

    Enable memtest86 in grub

--- nixos/celeborn_config.nix
+++ nixos/celeborn_config.nix
@@ -17,6 +17,7 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
+  boot.loader.grub.memtest86.enable = true;
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sdc";
   boot.loader.grub.extraEntries =
