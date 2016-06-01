8ba6f970a098e16c301a3216a686c336e93160c0
Spencer Janssen <spencerjanssen@gmail.com>
2016-05-31 22:34:05 -0500

    Surface config updates
    Include patch files in the repo

--- nixos/surface_config.nix
+++ nixos/surface_config.nix
@@ -12,23 +12,61 @@
 
   # Use the GRUB 2 boot loader.
   boot.loader.gummiboot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.gummiboot.timeout = 5;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  boot.resumeDevice = "/dev/nvme0n1p6";
+  boot.initrd.kernelModules = [ "nvme" ];
+  services.upower.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.enableAllFirmware = true;
+  services.logind.extraConfig = ''
+    HandleLidSwitch=hibernate
+    HandleLidSwitchDocked=ignore
+  '';
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    linux_4_4 = pkgs.linux_4_4.override {
+  boot.kernelPackages =
+    let self = pkgs.linuxPackagesFor (pkgs.callPackage <nixpkgs/pkgs/os-specific/linux/kernel/linux-4.6.nix> {
         kernelPatches =
+            let p = name: path: {patch = path; name = name;};
+            in
             [ pkgs.kernelPatches.bridge_stp_helper
-              { patch = /home/sjanssen/debian-mssp4/patches/001_typing-cover.patch;
-                name = "surface_touchpad"; }
+            pkgs.kernelPatches.qat_common_Makefile
+             #(p "cam" /home/sjanssen/Downloads/debian/patches/surface-cam.patch)
+             #(p "button" /home/sjanssen/Downloads/debian/patches/surface-button.patch)
+             #(p "button-config" /home/sjanssen/Downloads/debian/patches/surface-button-config.patch)
+             (p "cam" ./patches/Microsoft-Surface-Pro-4-Surface-Book-camera-support.patch)
+             (p "tc 3" ./patches/Add-multitouch-support-for-Microsoft-Type-Cover-3.patch)
+             (p "mt quirk" ./patches/HID-multitouch-Add-MT_QUIRK_NOT_SEEN_MEANS_UP-to-MT_.patch)
+             (p "mt ignore" ./patches/HID-multitouch-Ignore-invalid-reports.patch)
+	     # fix and uncomment later
+             #(p "surface-lid" /home/sjanssen/Downloads/debian/patches/surface-lid.patch)
+             # (p "surface-touchpad" /home/sjanssen/Downloads/debian/patches/surface-touchpad.patch)
             ];
-    };
-  };
+    }) self;
+    in self;
+
   nix.buildCores = 0;
+  nix.buildMachines =
+    [ {hostName = "192.168.86.200"; maxJobs = 1; sshUser = "sjanssen"; sshKey = "/home/sjanssen/.ssh/id_rsa"; system="x86_64-linux";}]
+    ;
+  nix.distributedBuilds = true;
 
   networking.hostName = "surface"; # Define your hostname.
 
   time.timeZone = "US/Central";
 
   services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.synaptics = {
+    enable = true;
+    palmDetect = true;
+    twoFingerScroll = true;
+  };
 
+  services.autossh.sessions =
+    [
+        { name = "sidev";
+        user = "sjanssen";
+        extraArguments = "-N -D 127.0.0.1:1234 -L 1337:127.0.0.1:1337 -L 8080:127.0.0.1:8080 -L 8081:127.0.0.1:8081 sidev";
+        }
+    ];
 }
