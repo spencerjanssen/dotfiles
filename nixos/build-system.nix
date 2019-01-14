1754d6a72a3cb49e73957c854477e119551c03d0
Spencer Janssen <spencerjanssen@gmail.com>
2019-01-14 10:39:37 -0600

    Add individual packages to Hydra job

--- nixos/build-system.nix
+++ nixos/build-system.nix
@@ -7,7 +7,8 @@ let ungoliantConfig =
                 {_module.args.dummyflexget = "don't build";}
             ];
         };
+    pkgs = import <nixpkgs> {};
 in
 {
     ungoliant = ungoliantConfig.config.system.build.toplevel;
-}
+} // pkgs.lib.lists.foldr (p: s: s // {"${p.name}" = p;}) {} ungoliantConfig.config.environment.systemPackages
