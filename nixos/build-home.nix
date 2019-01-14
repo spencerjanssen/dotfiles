c75851eac03a6b42cf4dcd4e7963cd9f50237763
Spencer Janssen <spencerjanssen@gmail.com>
2019-01-14 16:20:51 -0600

    Add individual packages to buildhome Hydra job

--- nixos/build-home.nix
+++ nixos/build-home.nix
@@ -1,10 +1,9 @@
 {}:
 let pkgs = import <nixpkgs> {config.allowUnfree = true;};
-    hm = import <home-manager/home-manager/home-manager.nix> {
-            inherit pkgs;
-            confPath = ./build-home-no-secrets-wrapper.nix;
-            confAttr = "";
-        };
+    hm = import <home-manager/modules/default.nix> {
+        inherit pkgs;
+        configuration = ./build-home-no-secrets-wrapper.nix;
+    };
 in {
     homebuilder = hm.activationPackage;
-}
+} // builtins.listToAttrs (map (p: {name = (builtins.parseDrvName p.name).name; value = p;}) hm.config.home.packages)
