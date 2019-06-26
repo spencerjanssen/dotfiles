3533c6ff9be315aeaab59fd87f4adf8d1d70dd65
Spencer Janssen <spencerjanssen@gmail.com>
2019-06-26 15:35:33 -0500

    Remove plugins from system xmonad which isn't used

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -60,10 +60,6 @@
     default = "xmonad";
     xmonad.enable = true;
     xmonad.enableContribAndExtras = true;
-    xmonad.extraPackages = haskellPackages: [
-      haskellPackages.xmonad-contrib
-      haskellPackages.taffybar
-    ];
   };
   services.xserver.desktopManager.xterm.enable = false;
   services.xserver.desktopManager.default = "none";
