9366371645ad845ee566b577c98fa003334b79ce
Spencer Janssen <spencerjanssen@gmail.com>
2019-08-08 16:52:51 -0500

    Update lorri

--- nixos/overlays.nix
+++ nixos/overlays.nix
@@ -12,8 +12,8 @@
             let lorriSource = super.fetchFromGitHub {
                 owner = "target";
                 repo = "lorri";
-                rev = "094a903d19eb652a79ad6e7db6ad1ee9ad78d26c";
-                sha256 = "0y9y7r16ki74fn0xavjva129vwdhqi3djnqbqjwjkn045i4z78c8";
+                rev = "d3e452ebc2b24ab86aec18af44c8217b2e469b2a";
+                sha256 = "07yf3gl9sixh7acxayq4q8h7z4q8a66412z0r49sr69yxb7b4q89";
             };
             in (import lorriSource {src = lorriSource;});
     })
