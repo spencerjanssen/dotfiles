c255d5a21a88f2cc7b98f917f1c4a6afbe414b61
Spencer Janssen <spencerjanssen@gmail.com>
2019-08-27 13:53:37 -0500

    Add home to spec

--- nixos/hydra/spec.nix
+++ nixos/hydra/spec.nix
@@ -1,34 +1,41 @@
 { nixpkgs, declInput }:
 let pkgs = import nixpkgs {};
-    js = {
-        ungoliant = {
-            enabled = 1;
-            hidden = false;
-            description = "js";
-            nixexprinput = "dotfiles";
-            nixexprpath = "nixos/build-system.nix";
-            checkinterval = 300;
-            schedulingshares = 100;
-            enableemail = false;
-            emailoverride = "spencerjanssen@gmail.com";
-            keepnr = 3;
-            inputs = {
-                dotfiles = {
-                    type = "git";
-                    value = "git://github.com/spencerjanssen/dotfiles.git decltest";
-                    emailresponsible = false;
-                };
-                nixpkgs = {
-                    type = "git";
-                    value = "git://github.com/NixOS/nixpkgs-channels nixos-unstable";
-                    emailresponsible = false;
-                };
+    common = {
+        enabled = 1;
+        hidden = false;
+        nixexprinput = "dotfiles";
+        checkinterval = 300;
+        schedulingshares = 100;
+        enableemail = false;
+        emailoverride = "spencerjanssen@gmail.com";
+        keepnr = 3;
+        inputs = {
+            dotfiles = {
+                type = "git";
+                value = "git://github.com/spencerjanssen/dotfiles.git decltest";
+                emailresponsible = false;
             };
+            nixpkgs = {
+                type = "git";
+                value = "git://github.com/NixOS/nixpkgs-channels nixos-unstable";
+                emailresponsible = false;
+            };
+        };
+    };
+
+    jobs = {
+        ungoliant = common // {
+            nixexprpath = "nixos/build-system.nix";
+            description = "Ungoliant system configuration";
+        };
+        home = common // {
+            nixexprpath = "nixos/build-home.nix";
+            description = "home-manager configuration";
         };
     };
     json = pkgs.writeTextFile {
         name = "spec.json";
-        text = builtins.toJSON js;
+        text = builtins.toJSON jobs;
     };
 in {
     jobsets = json;
