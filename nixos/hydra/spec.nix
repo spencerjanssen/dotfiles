429521558866bd0fdd36420b0fadb02a0fa0365f
Spencer Janssen <spencerjanssen@gmail.com>
2019-08-27 13:38:44 -0500

    Use expressions instead of JSON

--- nixos/hydra/spec.nix
+++ nixos/hydra/spec.nix
@@ -1,37 +1,30 @@
 { nixpkgs, declInput }: let pkgs = import nixpkgs {};
 in {
-    jobsets = pkgs.runCommand "spec.json" {} ''
-        cat <<EOF
-        ${builtins.toXML declInput}
-        EOF
-        cat > $out <<EOF
-        {
-            "ungoliant": {
-                "enabled": 1,
-                "hidden": false,
-                "description": "js",
-                "nixexprinput": "dotfiles",
-                "nixexprpath": "nixos/build-system.nix",
-                "checkinterval": 300,
-                "schedulingshares": 100,
-                "enableemail": false,
-                "emailoverride": "spencerjanssen@gmail.com",
-                "keepnr": 3,
-                "inputs": {
-                    "dotfiles": {
-                        "type": "git",
-                        "value": "git://github.com/spencerjanssen/dotfiles.git decltest",
-                        "emailresponsible": false
-                    },
-                    "nixpkgs": {
-                        "type": "git",
-                        "value": "git://github.com/NixOS/nixpkgs-channels nixos-unstable",
-                        "emailresponsible": false
-                    }
-                }
-            }
+    jobsets = builtins.toJSON {
+        ungoliant = {
+            enabled = 1;
+            hidden = false;
+            description = "js";
+            nixexprinput = "dotfiles";
+            nixexprpath = "nixos/build-system.nix";
+            checkinterval = 300;
+            schedulingshares = 100;
+            enableemail = false;
+            emailoverride = "spencerjanssen@gmail.com";
+            keepnr = 3;
+            inputs = {
+                dotfiles = {
+                    type = "git";
+                    value = "git://github.com/spencerjanssen/dotfiles.git decltest";
+                    emailresponsible = false
+                };
+                nixpkgs = {
+                    type = "git";
+                    value = "git://github.com/NixOS/nixpkgs-channels nixos-unstable";
+                    emailresponsible = false
+                };
+            };
         }
-        EOF
-    '';
+    };
 
 }
