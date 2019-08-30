48cc78e3f657ade62c8d7cf5969cd41b9c4223bf
Spencer Janssen <spencerjanssen@gmail.com>
2019-08-30 14:19:32 -0500

    Remove cross compiling hacks

--- nixos/imladris/configuration.nix
+++ nixos/imladris/configuration.nix
@@ -9,12 +9,7 @@
     <home-manager/nixos>
   ];
 
-  # Tell the host system that it can, and should, build for aarch64.
-  nixpkgs = rec {
-    crossSystem = (import <nixpkgs> {}).pkgsCross.aarch64-multiplatform.stdenv.targetPlatform;
-    localSystem = crossSystem;
-    overlays = import ../common/overlays.nix;
-  };
+  nixpkgs.overlays = import ../common/overlays.nix;
 
   fileSystems = {
     "/" = {
@@ -31,8 +26,6 @@
 
   hardware.enableRedistributableFirmware = true;
 
-  # For the ugly hack to run the activation script in the chroot'd host below. Remove after sd card is set up.
-  # environment.etc."binfmt.d/nixos.conf".text = ":aarch64:M::\\x7fELF\\x02\\x01\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x02\\x00\\xb7\\x00:\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x00\\xff\\xff\\xff\\xff\\xff\\xff\\x00\\xff\\xfe\\xff\\xff\\xff:/run/binfmt/aarch64:";
   boot= {
     kernelPackages = pkgs.linuxPackages_rpi4;
     loader = {
