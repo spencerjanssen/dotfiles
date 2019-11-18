5290ba7024e2059193d583ad6294023e18cf1aaa
Spencer Janssen <spencerjanssen@gmail.com>
2019-11-18 08:47:07 -0600

    Enable more sysrq functions

--- nixos/ungoliant/config.nix
+++ nixos/ungoliant/config.nix
@@ -93,6 +93,16 @@
     fallbackToPassword = true;
   };
 
+  boot.kernel.sysctl = {
+    "kernel.sysrq" =
+      4 # keyboard control
+      + 16 # sync
+      + 32 # remount read-only
+      + 64 # signal processes
+      + 128 # reboot/poweroff
+    ;
+  };
+
   fileSystems."/media/spinning".options = ["noauto"];
 
   services.xserver.videoDrivers = [ "amdgpu" ];
