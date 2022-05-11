9c67859ec3c4ef9780f3639e1e67688aec285fef
Spencer Janssen <spencerjanssen@gmail.com>
2022-05-11 11:03:13 -0500

    Hydra GitHub build status

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,4 +3,5 @@ let sjanssen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIECSFC1NwzaUzuK6r++R6INw2GDd
 in
 {
     "sjanssen-ssh-config.age".publicKeys = [sjanssen ungoliant];
+    "hydra-github-token.age".publicKeys = [sjanssen ungoliant];
 }
