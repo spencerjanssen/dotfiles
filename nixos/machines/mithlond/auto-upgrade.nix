533ee8622ca3f7f2461da02927527a750034b3b8
Spencer Janssen <spencerjanssen@gmail.com>
2025-05-19 13:17:05 -0500

    Eagerly trigger upgrades

--- nixos/machines/mithlond/auto-upgrade.nix
+++ nixos/machines/mithlond/auto-upgrade.nix
@@ -4,5 +4,11 @@
     enable = true;
     flake = "github:spencerjanssen/dotfiles";
     allowReboot = true;
+    # 04:40 is the default. Explicitly written out to ensure it's within the reboot window.
+    dates = "04:40";
+    rebootWindow = {
+      lower = "00:00";
+      upper = "06:00";
+    };
   };
 }
--- nixos/services/github-runner.nix
+++ nixos/services/github-runner.nix
@@ -1,5 +1,6 @@
 { config, ... }:
 {
+  users.groups.dotfiles-builder = { };
   services.github-runners.dotfiles-builder = {
     enable = true;
     ephemeral = true;
@@ -7,9 +8,23 @@
     extraLabels = [ "dotfiles-builder" ];
     replace = true;
     tokenFile = config.age.secrets.dotfiles-builder-github-token.path;
+    serviceOverrides = {
+      SupplementaryGroups = [ "dotfiles-builder" ];
+    };
   };
   age.secrets = {
     dotfiles-builder-github-token.file = ../../secrets/dotfiles-builder-github-token.age;
   };
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        action.id == "org.freedesktop.systemd1.manage-units" &&
+        action.lookup("unit") == "nixos-upgrade.timer" &&
+        subject.isInGroup("dotfiles-builder")
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
 
