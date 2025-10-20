8802d6d092310cb9e038cecbbacd8def2a467fa5
Spencer Janssen <spencerjanssen@gmail.com>
2025-10-20 13:51:51 -0500

    starship tweaks

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -25,8 +25,30 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      time = { disabled = false; };
-      direnv = { disabled = false; };
+      direnv = {
+        disabled = false;
+        format = "[$symbol$loaded]($style) ";
+        loaded_msg = "️✔";
+        unloaded_msg = "✘";
+      };
+      nix_shell.format = "[$symbol]($style)";
+      haskell.disabled = true;
+      git_branch.format = "[$symbol$branch]($style) ";
+      git_metrics.disabled = false;
+      directory = {
+        truncate_to_repo = false;
+        fish_style_pwd_dir_length = 1;
+        before_repo_root_style = "dimmed";
+        repo_root_style = "cyan";
+      };
+      character = {
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[✗](bold red)";
+      };
+      status = {
+        disabled = false;
+        format = "[\\[$common_meaning$signal_name$maybe_int\\]]($style) ";
+      };
     };
   };
 }
