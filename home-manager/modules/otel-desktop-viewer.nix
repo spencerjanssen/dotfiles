d4822cf06c6e490808e7a7bf9d8b9d044dbc0054
Spencer Janssen <spencerjanssen@gmail.com>
2026-09-08 14:17:05 -0500

    Persist otel-desktop-viewer DB, don't open browser

--- home-manager/modules/otel-desktop-viewer.nix
+++ home-manager/modules/otel-desktop-viewer.nix
@@ -5,8 +5,9 @@
       Description = "otel-desktop-viewer";
     };
     Service = {
-      ExecStart = "${pkgs.otel-desktop-viewer}/bin/otel-desktop-viewer";
+      ExecStart = "${pkgs.otel-desktop-viewer}/bin/otel-desktop-viewer --open-browser=false --db %C/otel-desktop-viewer/otel-desktop-viewer.db --db-max-size=512MB";
       Restart = "on-failure";
+      CacheDirectory = "otel-desktop-viewer";
     };
     Install = {
       WantedBy = [ "default.target" ];
